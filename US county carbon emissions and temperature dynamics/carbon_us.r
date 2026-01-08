3f3d19b97ae3dd6efb561ed45af4aa74044377ca
SpatLyu <lyu.geosocial@gmail.com>
2026-01-08 14:19:35 +0800

    update carbon case

--- US county carbon emissions and temperature dynamics/carbon_us.r
+++ US county carbon emissions and temperature dynamics/carbon_us.r
@@ -6,12 +6,16 @@ head(carbon)
 carbon_list = dplyr::group_split(carbon, by = fips)
 length(carbon_list)
 
+purrr::map(carbon_list,
+           \(.x) tEDM::fnn(.x, "carbon", E = 2:10,
+                           eps = stats::sd(.x$carbon))) -> carbon_fnn
+
 tEDM::fnn(carbon_list[[100]],"carbon",E = 2:10,
           eps = stats::sd(carbon_list[[100]]$carbon))
 
 res = carbon_list |>
   purrr::map_dfr(\(.x) {
-    g = tEDM::cmc(.x,"tem","carbon",E = 3,k = 20,dist.metric = "L2",progressbar = FALSE)
+    g = tEDM::cmc(.x,"tem","carbon",E = 3,k = 18,dist.metric = "L2",progressbar = FALSE)
     return(g$xmap)
   })
 head(res)
@@ -34,11 +38,11 @@ fig_county_us = ggplot2::ggplot(res_carbon,
   ggplot2::scale_x_discrete(name = "") +
   ggplot2::scale_y_continuous(name = "Causal Strength",
                               expand = c(0,0),
-                              limits = c(0,0.3),
-                              breaks = seq(0,0.3,by = 0.05)) +
+                              limits = c(0,0.45),
+                              breaks = seq(0,0.45,by = 0.05)) +
   ggplot2::theme(legend.position = "none")
-
 fig_county_us + ggview::canvas(4.5,4.5,dpi = 300)
+
 ggview::save_ggplot(fig_county_us + ggview::canvas(4.5,4.5,dpi = 300),
                     "./US county carbon emissions and temperature dynamics/carbon_us.pdf",
                     device = cairo_pdf)
