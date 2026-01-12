6a45ea8473231bd71679533d16503e6c0c4fadac
SpatLyu <lyu.geosocial@gmail.com>
2026-01-12 18:30:44 +0800

    draft supplementary analysis

--- US county carbon emissions and temperature dynamics/multispatial_application.r
+++ US county carbon emissions and temperature dynamics/multispatial_application.r
@@ -78,46 +78,46 @@ ggview::save_ggplot(fig_ccm + ggview::canvas(4.5,4.5,dpi = 300),
 
 nb = spdep::read.gal('./US county carbon emissions and temperature dynamics/us_county.gal')
 
-res_gc = carbon_list |>
-  purrr::map_dfr(\(.x) {
-    f1 = lmtest::grangertest(tem ~ carbon, order = 3, data = .x)
-    f2 = lmtest::grangertest(carbon ~ tem, order = 3, data = .x)
-    return(tibble::tibble(
-      carbon_tem = f1$`Pr(>F)`[2],
-      tem_carbon = f2$`Pr(>F)`[2]
-    ))
+res_multispatialccm = seq_along(carbon_list) |> 
+  purrr::map_dfr(\(.i) {
+    obs = carbon_list[c(nb[[.i]],.i)]
+    suppressMessages({
+      obs_plot = list("tem" = purrr::map_dfc(obs,\(.x) dplyr::select(.x,tem)), 
+                      "carbon" = purrr::map_dfc(obs,\(.x) dplyr::select(.x,carbon)))
+    })
+    g = tEDM::multispatialccm(obs_plot,"tem","carbon",libsizes = length(obs),E = 3,k = 4,
+                              boot = 1, dist.metric = "L2", progressbar = FALSE)
+    return(g$xmap)
   }) |>
+  dplyr::select(libsizes,
+                carbon_tem = x_xmap_y_mean,
+                tem_carbon = y_xmap_x_mean) |>
   tidyr::pivot_longer(c(carbon_tem, tem_carbon),
-                      names_to = "variable",
+                      names_to = "variable", 
                       values_to = "value")
-res_gc$variable = factor(res_gc$variable,
-                         levels = c("carbon_tem", "tem_carbon"),
-                         labels = c("carbon → tem", "tem → carbon"))
-head(res_gc)
-readr::write_rds(res_gc,"./US county carbon emissions and temperature dynamics/res_gc.rds")
 
-res_gc = readr::read_rds("./US county carbon emissions and temperature dynamics/res_gc.rds")
-fig_gc = ggplot2::ggplot(res_gc,
-                         ggplot2::aes(x = variable, y = value, fill = variable)) +
-  ggplot2::geom_violin(trim = FALSE, alpha = 0.7) + 
-  ggplot2::geom_boxplot(width = 0.2, fill = "white", outlier.shape = NA) +  
-  ggplot2::geom_hline(yintercept = 0.05, linetype = "dashed", color = "red", linewidth = 0.8) +
+res_multispatialccm$variable = factor(res_multispatialccm$variable,
+                                      levels = c("carbon_tem", "tem_carbon"),
+                                      labels = c("carbon → tem", "tem → carbon"))
+head(res_multispatialccm)
+readr::write_rds(res_multispatialccm,"./US county carbon emissions and temperature dynamics/res_multispatialccm.rds")
+
+res_multispatialccm = readr::read_rds("./US county carbon emissions and temperature dynamics/res_multispatialccm.rds")
+fig_multispatialccm = ggplot2::ggplot(res_multispatialccm,
+                                      ggplot2::aes(x = variable, y = value, fill = variable)) +
+  ggplot2::geom_boxplot() +
+  ggplot2::geom_hline(yintercept = 0.2, linetype = "dashed", color = "red", linewidth = 0.8) +
   ggplot2::theme_bw() +
   ggplot2::scale_x_discrete(name = "") +
-  ggplot2::scale_y_continuous(
-    name = "P Value for Granger Causality",
-    expand = c(0, 0),
-    limits = c(0, 1),
-    breaks = c(0, 0.05, 0.1, 0.5, 1) 
-  ) +
-  ggplot2::theme(
-    legend.position = "none",
-    panel.grid.minor = ggplot2::element_blank(),
-    axis.text.x = ggplot2::element_text(size = 12),
+  ggplot2::scale_y_continuous(name = "Cross Mapping Skill",
+                              expand = c(0,0),
+                              limits = c(-1,1),
+                              breaks = seq(-1,1,by = 0.2)) +
+  ggplot2::theme(legend.position = "none",
+                 axis.text.x = ggplot2::element_text(size = 12),
                  axis.text.y = ggplot2::element_text(size = 12),
-                 axis.title.y = ggplot2::element_text(size = 12.5)
-  )
-fig_gc + ggview::canvas(4.5, 4.5, dpi = 300)
-ggview::save_ggplot(fig_gc + ggview::canvas(4.5, 4.5, dpi = 300),
-                    "./US county carbon emissions and temperature dynamics/carbon_us_gc.pdf",
+                 axis.title.y = ggplot2::element_text(size = 12.5))
+fig_multispatialccm + ggview::canvas(4.5,4.5,dpi = 300)
+ggview::save_ggplot(fig_ccm + ggview::canvas(4.5,4.5,dpi = 300),
+                    "./US county carbon emissions and temperature dynamics/carbon_us_ccm.pdf",
                     device = cairo_pdf)
