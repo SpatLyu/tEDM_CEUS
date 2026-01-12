df4f3fc5c12739ac5467dc21af60b743901e79d1
SpatLyu <lyu.geosocial@gmail.com>
2026-01-12 17:07:55 +0800

    draft supplementary analysis

--- US county carbon emissions and temperature dynamics/multispatial_application.r
+++ US county carbon emissions and temperature dynamics/multispatial_application.r
@@ -7,7 +7,19 @@ head(carbon)
 #------               Convergent Cross Mapping analysis                 ------#
 #-----------------------------------------------------------------------------#
 
+carbon = carbon |> 
+  dplyr::mutate(fips = as.character(fips)) |> 
+  dplyr::mutate(fips = stringr::str_pad(fips, width = 5, side = "left", pad = "0"))
+
+us_counties = tigris::counties(resolution = "20m", year = 2017)
+us_counties = us_counties |> 
+  dplyr::select(fips = GEOID, name = NAME) |> 
+  dplyr::filter(fips %in% carbon$fips) |> 
+  dplyr::arrange(fips)
+plot(sf::st_geometry(us_counties))
+
 carbon_list = dplyr::group_split(carbon, by = fips)
+names(carbon_list) = purrr::map_chr(carbon_list, \(.l) unique(.l$fips))
 length(carbon_list)
 
 # purrr::map(carbon_list,
